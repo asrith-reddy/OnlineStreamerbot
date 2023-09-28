ded23498f44a2f6bdbbd075bac23179532a25523
Asrith <107095636+Asrith52@users.noreply.github.com>
2023-09-28 15:32:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1589,7 +1589,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"⚡Here is what i found for your query {search}\n[Click Here if you didnt get file](https://telegram.me/Movie_Streamer_bot?start=files)\n<b>@Movie_Streamer_bot</b>"
+        cap = f"⚡Here is what i found for your query {search}\n\n<b>[Please start the bot to get files](https://telegram.me/Movie_Streamer_bot?start=files)</b.\n@Movie_Streamer_bot"
     if imdb and imdb.get('poster'):
         try:
             z = await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
